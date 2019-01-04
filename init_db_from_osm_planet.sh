9aaf821d6c3edd0b7f000c5bb4659cc0f49fb912
MyrikLD <myrik260138@tut.by>
2019-01-04 12:48:48 +0300

    Update init_db_from_osm_planet.sh

--- init_db_from_osm_planet.sh
+++ init_db_from_osm_planet.sh
@@ -2,6 +2,19 @@
 
 source conf.sh
 
+function init_planet {
+	#Download the planet
+	echo "Will start downloading the planet file at $(date)"
+	echo "From: $PLANET_FILE"
+	wget --quiet -O planet.osm.bz2 $PLANET_FILE
+	echo "Planet file downloaded at $(date)"
+
+	#init the planet
+	echo "Will init the import of the planet. Started at $(date)"
+	$BINDIR/init_osm3s.sh $DBDIR/planet.osm.bz2 $DBDIR $EXECDIR --meta
+	echo "Import finished at $(date)"
+}
+
 space=`du -s $DBDIR | cut -f1`
 if [ ! -e $DBDIR/nodes.bin ]; then
 	#Database doesn't exist
@@ -9,16 +22,8 @@ if [ ! -e $DBDIR/nodes.bin ]; then
 
 	#Get actual last replication state
 	wget -O /dev/shm/state.txt "$REPLICATE_SERVER/state.txt"
-
-	#Download the planet
-	echo "Will start downloading the planet file at $(date)"
-	wget --quiet -O planet.osm.bz2 $PLANET_FILE
-	echo "Planet file downloaded at $(date)"
-
-	#init the planet
-	echo "Will init the import of the planet. Can take ~40hours. Started at $(date)"
-	$BINDIR/init_osm3s.sh $DBDIR/planet.osm.bz2 $DBDIR $EXECDIR --meta
-	echo "Import finished at $(date)"
+	
+	init_planet
 
 	#Get 7 days of backwards history; we never know when was the planet.osm exported..
 	NUM=`cat /dev/shm/state.txt | grep sequenceNumber | cut -d'=' -f2`
